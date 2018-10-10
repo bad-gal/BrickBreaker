94856ea5e8d148901acda282f845683a5de109fb
Andrien Ricketts <andrien26@gmail.com>
2018-10-10 21:51:11 +0100

    Implement ball movement methods. Identify whether the ball has fallen off-screen

--- lib/ball.rb
+++ lib/ball.rb
@@ -1,13 +1,47 @@
 class Ball
   include Image
-  attr_reader :position, :image
+  attr_accessor :position, :velocity
+  attr_reader :image
 
   def initialize(file:, position:)
     @position = position
+    @velocity = [0, 0]
     @image = Image.create(file: file)
   end
 
   def draw
     Image.draw(image: @image, position: @position, z: 0)
+  end
+
+  def move_left
+    return if @position[0] < Settings::RB_CENTRE_ON_PADDLE + Settings::PADDLE_MOVE
+
+    @position[0] -= Settings::PADDLE_MOVE
+  end
+
+  def move_right
+    return if @position[0] > Settings::SCREEN_WIDTH - Settings::PADDLE_MOVE -
+                             (Settings::PADDLE_WIDTH - Settings::RB_CENTRE_ON_PADDLE)
+
+    @position[0] += Settings::PADDLE_MOVE
+  end
+
+  def lift_off
+    @velocity[1] = -Settings::PADDLE_MOVE
+  end
+
+  def move
+    @position[0] += @velocity[0]
+    @position[1] += @velocity[1]
+  end
+
+  def bounce_off
+    @velocity[1] = -@velocity[1] if @position[1] <= 0
+    @velocity[0] = -@velocity[0] if @position[0] <= 0
+    @velocity[0] = -@velocity[0] if @position[0] + Settings::REGULAR_BALL_AREA >= Settings::SCREEN_WIDTH
+  end
+
+  def lost?
+    @position[1] > Settings::SCREEN_HEIGHT
   end
 end
--- spec/ball_spec.rb
+++ spec/ball_spec.rb
@@ -2,11 +2,102 @@ require 'image'
 require 'ball'
 
 RSpec.describe Ball do
-  let(:ball) { Ball.new(file: 'image.png', position: [100, 100]) }
+  let(:ball) { Ball.new(file: 'image.png', position: [350, 400]) }
 
   describe '.initialize' do
     it 'is valid with the correct parameters' do
       expect(ball).to be_truthy
+    end
+  end
+
+  describe '.move_left' do
+    context 'ball horizontal position is more than 5' do
+      it 'moves the ball position left' do
+        ball.move_left
+        expect(ball.position).to eq([345, 400])
+      end
+    end
+    context 'ball horizontal position is less than 5' do
+      before do
+        ball.position = [0, 400]
+        ball.move_left
+      end
+      it 'does not move the ball to the left' do
+        expect(ball.position).to eq([0, 400])
+      end
+    end
+  end
+
+  describe '.move_right' do
+    context 'ball horizontal position is less than screen boundary' do
+      it 'moves the ball position right' do
+        ball.move_right
+        expect(ball.position).to eq([355, 400])
+      end
+    end
+    context 'ball horizontal position is more than screen boundary' do
+      before do
+        ball.position = [640, 400]
+        ball.move_right
+      end
+      it 'does not move the ball to the right' do
+        expect(ball.position).to eq([640, 400])
+      end
+    end
+  end
+
+  describe '.lift_off' do
+    context 'ball vertical position is more than 0' do
+      it 'moves the ball upwards' do
+        ball.lift_off
+        expect(ball.velocity).to eq([0, -Settings::PADDLE_MOVE])
+      end
+    end
+  end
+
+  describe '.bounce_off' do
+    context 'when ball collides with the boundary' do
+      context 'at the top of the screen' do
+        before do
+          ball.position[1] = 0
+          ball.velocity[1] = -Settings::PADDLE_MOVE
+        end
+        it 'changes its velocity' do
+          ball.bounce_off
+          expect(ball.velocity[1]).to eq(Settings::PADDLE_MOVE)
+        end
+      end
+      context 'at the left of the screen' do
+        before do
+          ball.position[0] = 0
+          ball.velocity[0] = -Settings::PADDLE_MOVE
+        end
+        it 'changes its velocity' do
+          ball.bounce_off
+          expect(ball.velocity[0]).to eq(Settings::PADDLE_MOVE)
+        end
+      end
+      context 'at the right of the screen' do
+        before do
+          ball.position[0] = 635
+          ball.velocity[0] = Settings::PADDLE_MOVE
+        end
+        it 'changes its velocity' do
+          ball.bounce_off
+          expect(ball.velocity[0]).to eq(-Settings::PADDLE_MOVE)
+        end
+      end
+    end
+  end
+
+  describe '.lost' do
+    context 'when ball has not been caught by paddle' do
+      before do
+        ball.position = [56, 490]
+      end
+      it 'returns true that ball is lost' do
+        expect(ball.lost?).to eq(true)
+      end
     end
   end
 end
