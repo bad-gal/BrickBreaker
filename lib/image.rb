9963100de2345b4404754cc3722b669b248a7522
Andrien Ricketts <knowledgehut.developments@gmail.com>
2018-11-16 12:47:51 +0000

    Draw method should only be considered with x and y values as z not applicable for 2d game

--- lib/image.rb
+++ lib/image.rb
@@ -1,10 +1,12 @@
+require 'gosu'
+
 module Image
 
   def self.create(file:)
     Gosu::Image.new(file, tileable: true)
   end
 
-  def self.draw(image:, position:, z:)
-    image.draw(position.first, position.last, z)
+  def self.draw(image:, position:)
+    image.draw(position[:x], position[:y], 0)
   end
 end
