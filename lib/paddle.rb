c55cede01441bec789788a92c5bc30893cca3c53
Andrien Ricketts <knowledgehut.developments@gmail.com>
2018-11-16 19:15:18 +0000

    Changed the way the paddle class operates

--- lib/paddle.rb
+++ lib/paddle.rb
@@ -1,66 +1,95 @@
+require_relative 'state'
 require_relative 'settings'
 
 class Paddle
   include Image
-  attr_accessor :position, :width, :height, :wrap
-  attr_reader :image
+  attr_accessor :width, :height, :position, :state
+  attr_reader :image, :action, :gun
 
-  SMALL_PADDLE = { file: 'assets/paddle_small.png', pixel_size: 61 }.freeze
-  REGULAR_PADDLE = { file: 'assets/paddle_simple.png', pixel_size: 80 }.freeze
-  LONG_PADDLE = { file: 'assets/paddle_large.png', pixel_size: 100 }.freeze
+  SMALL_PADDLE = { file: 'assets/paddle_small.png', width: 61 }.freeze
+  REGULAR_PADDLE = { file: 'assets/paddle_simple.png', width: 80 }.freeze
+  LARGE_PADDLE = { file: 'assets/paddle_large.png', width: 100 }.freeze
+  HEIGHT = 16
+  NORMAL_ACTION = :normal
+  WRAP_ACTION = :wrap
+  FLIP_ACTION = :flip
 
-  def initialize(position = [0, 0], details:)
-    @position = position
-    @image = Image.create(file: details[:file])
-    @width = details[:pixel_size]
-    @height = 16
-    @wrap = false
+  def initialize
+    reset
+  end
+
+  def reset
+    @image = Image.create(file: REGULAR_PADDLE[:file])
+    @width = REGULAR_PADDLE[:width]
+    @height = HEIGHT
+    @state = State::BALL_IN_PADDLE
+    @action = NORMAL_ACTION
+    x = (Settings::GAME_WIDTH / 2) - (@width / 2)
+    y =  Settings::GAME_HEIGHT - @height
+    @position = { x: x, y: y }
+    @gun = false
   end
 
   def draw
-    Image.draw(image: @image, position: @position, z: 0)
+    Image.draw(image: @image, position: @position)
   end
 
   def move_left
-    if @wrap
-      wrap_left
-    else
-      return if @position[0] < Settings::PADDLE_MOVE
-
-      @position[0] -= Settings::PADDLE_MOVE
-    end
+    method(@action.to_s + '_left').call
   end
 
   def move_right
-    if @wrap
-      wrap_right
-    else
-      return if @position[0] > ((Settings::GAME_WIDTH - Settings::PADDLE_MOVE) -
-          Settings::PADDLE_WIDTH)
+    method(@action.to_s + '_right').call
+  end
 
-      @position[0] += Settings::PADDLE_MOVE
-    end
+  def normal_left
+    return if @position[:x] < Settings::PADDLE_MOVE
+
+    @position[:x] -= Settings::PADDLE_MOVE
+  end
+
+  def normal_right
+    return if @position[:x] > ((Settings::GAME_WIDTH - Settings::PADDLE_MOVE) - @width)
+
+    @position[:x] += Settings::PADDLE_MOVE
   end
 
   def wrap_left
-    if @position[0] < Settings::PADDLE_MOVE
-      @position[0] = (Settings::GAME_WIDTH - Settings::PADDLE_MOVE) - Settings::PADDLE_WIDTH
+    if @position[:x] < Settings::PADDLE_MOVE
+      @position[:x] = (Settings::GAME_WIDTH - Settings::PADDLE_MOVE) - @width
     else
-      @position[0] -= Settings::PADDLE_MOVE
+      @position[:x] -= Settings::PADDLE_MOVE
     end
   end
 
   def wrap_right
-    if @position[0] > ((Settings::GAME_WIDTH - Settings::PADDLE_MOVE) -
-        Settings::PADDLE_WIDTH)
-      @position[0] = Settings::PADDLE_MOVE
+    if @position[:x] > ((Settings::GAME_WIDTH - Settings::PADDLE_MOVE) - @width)
+      @position[:x] = Settings::PADDLE_MOVE
     else
-      @position[0] += Settings::PADDLE_MOVE
+      @position[:x] += Settings::PADDLE_MOVE
     end
   end
 
-  def change(details:)
-    @image = Image.create(file: details[:file])
-    @width = details[:pixel_size]
+  def flip_left
+    normal_right
+  end
+
+  def flip_right
+    normal_left
+  end
+
+  def reduce
+    @image = Image.create(file: SMALL_PADDLE[:file])
+    @width = SMALL_PADDLE[:width]
+  end
+
+  def enlarge
+    @image = Image.create(file: LARGE_PADDLE[:file])
+    @width = LARGE_PADDLE[:width]
+  end
+
+  def collides_with?(pos, width, height)
+    Settings.overlapping?(pos[:x], pos[:x] + width, @position[:x], @position[:x] + @width) &&
+        Settings.overlapping?(pos[:y], pos[:y] + height, @position[:y], @position[:y] + @height)
   end
 end
--- spec/paddle_spec.rb
+++ spec/paddle_spec.rb
@@ -1,9 +1,31 @@
 require 'image'
 require 'paddle'
+require 'capsule'
 
 RSpec.describe Paddle do
   let(:paddle_asset) { { file: 'image.png', pixel_size: 90 } }
-  let(:paddle) { Paddle.new([300, 200], details: paddle_asset) }
+  let(:paddle) { Paddle.new }
+
+  describe '.reset' do
+    it 'sets paddle position' do
+      expect(paddle.position).to eq(x: 280, y: 464)
+    end
+    it 'sets image width' do
+      expect(paddle.width).to eq(Paddle::REGULAR_PADDLE[:width])
+    end
+    it 'sets image height' do
+      expect(paddle.height).to eq(Paddle::HEIGHT)
+    end
+    it 'sets paddle state to ball_in_paddle' do
+      expect(paddle.state).to eq(State::BALL_IN_PADDLE)
+    end
+    it 'sets paddle action to normal' do
+      expect(paddle.action).to eq(Paddle::NORMAL_ACTION)
+    end
+    it 'sets gun to false' do
+      expect(paddle.gun).to eq(false)
+    end
+  end
 
   describe '.initialize' do
     it 'is valid with the correct parameters' do
@@ -15,16 +37,16 @@ RSpec.describe Paddle do
     context 'paddle horizontal position is more than 5' do
       it 'moves the paddle position left' do
         paddle.move_left
-        expect(paddle.position).to eq([295, 200])
+        expect(paddle.position).to eq(x: 275, y: 464)
       end
     end
     context 'paddle horizontal position is less than 5' do
       before do
-        paddle.position = [0, 200]
+        paddle.position = { x: 0, y: 200 }
         paddle.move_left
       end
       it 'does not move the paddle to the left' do
-        expect(paddle.position).to eq([0, 200])
+        expect(paddle.position).to eq(x: 0, y: 200)
       end
     end
   end
@@ -33,35 +55,148 @@ RSpec.describe Paddle do
     context 'paddle horizontal position is less than screen boundary' do
       it 'moves the paddle position right' do
         paddle.move_right
-        expect(paddle.position).to eq([305, 200])
+        expect(paddle.position).to eq(x: 285, y: 464)
       end
     end
     context 'paddle horizontal position is more than screen boundary' do
       before do
-        paddle.position = [640, 200]
+        paddle.position = { x: 640, y: 200 }
         paddle.move_right
       end
       it 'does not move the paddle to the right' do
-        expect(paddle.position).to eq([640, 200])
+        expect(paddle.position).to eq(x: 640, y: 200)
       end
     end
   end
 
-  describe '.change' do
-    context 'when paddle size changed to small' do
-      let(:small) { { file: 'image.png', pixel_size: 60 } }
+  describe '.wrap_left' do
+    context 'when paddle is at the far left of the screen' do
+      before do
+        paddle.position[:x] = 3
+        paddle.state = State::PLAYING
+        paddle.wrap_left
+      end
 
-      it 'is successfully changed' do
-        paddle.change(details: small)
-        expect(paddle.width).to eq(60)
+      it 'wraps the paddle to the far right of the screen' do
+        expect(paddle.position[:x]).to eq(Settings::GAME_WIDTH - Settings::PADDLE_MOVE  - paddle.width)
       end
     end
-    context 'when paddle size changed to large' do
-      let(:large) { { file: 'image.png', pixel_size: 120 } }
 
-      it 'is successfully changed' do
-        paddle.change(details: large)
-        expect(paddle.width).to eq(120)
+    context 'when paddle is not near the far left of the screen' do
+      before do
+        paddle.state = State::PLAYING
+        paddle.wrap_left
+      end
+      it 'moves the paddle to the left' do
+        expect(paddle.position[:x]).to eq(275)
+      end
+    end
+  end
+
+  describe '.wrap_right' do
+    context 'when paddle is at the far right of the screen' do
+      before do
+        paddle.position[:x] = Settings::GAME_WIDTH - 3
+        paddle.state = State::PLAYING
+        paddle.wrap_right
+      end
+
+      it 'wraps the paddle to the far left of the screen' do
+        expect(paddle.position[:x]).to eq(Settings::PADDLE_MOVE)
+      end
+    end
+
+    context 'when paddle is not near the far right of the screen' do
+      before do
+        paddle.state = State::PLAYING
+        paddle.wrap_right
+      end
+      it 'moves the paddle to the right' do
+        expect(paddle.position[:x]).to eq(285)
+      end
+    end
+  end
+
+  describe '.flip_left' do
+    context 'paddle horizontal position is less than screen boundary' do
+      it 'moves the paddle position right' do
+        paddle.flip_left
+        expect(paddle.position).to eq(x: 285, y: 464)
+      end
+    end
+    context 'paddle horizontal position is more than screen boundary' do
+      before do
+        paddle.position = { x: 640, y: 200 }
+        paddle.flip_left
+      end
+      it 'does not move the paddle to the right' do
+        expect(paddle.position).to eq(x: 640, y: 200)
+      end
+    end
+  end
+
+  describe '.flip_right' do
+    context 'paddle horizontal position is more than 5' do
+      it 'moves the paddle position left' do
+        paddle.flip_right
+        expect(paddle.position).to eq(x: 275, y: 464)
+      end
+    end
+    context 'paddle horizontal position is less than 5' do
+      before do
+        paddle.position = { x: 0, y: 200 }
+        paddle.flip_right
+      end
+      it 'does not move the paddle to the left' do
+        expect(paddle.position).to eq(x: 0, y: 200)
+      end
+    end
+  end
+
+  describe '.reduce' do
+    context 'when paddle size is reduced' do
+      before do
+        paddle.reduce
+      end
+
+      it 'makes the width of the paddle smaller' do
+        expect(paddle.width).to eq(Paddle::SMALL_PADDLE[:width])
+      end
+    end
+  end
+
+  describe '.enlarge' do
+    context 'when paddle size is increased' do
+      before do
+        paddle.enlarge
+      end
+
+      it 'makes the width of the paddle larger' do
+        expect(paddle.width).to eq(Paddle::LARGE_PADDLE[:width])
+      end
+    end
+  end
+
+  describe '.collides_with?' do
+    let(:capsule) { Capsule.new(type: :score_250, x: 310, y: 467) }
+
+    context 'when a collision does not occur' do
+      before do
+        paddle.position[:x] = 29
+      end
+
+      it 'returns false' do
+        expect(paddle.collides_with?(capsule.position, Capsule::CAPSULE_WIDTH, Capsule::CAPSULE_HEIGHT)).to eq(false)
+      end
+    end
+
+    context 'when a collision occurs' do
+      before do
+        paddle.reset
+      end
+
+      it 'returns true' do
+        expect(paddle.collides_with?(capsule.position, Capsule::CAPSULE_WIDTH, Capsule::CAPSULE_HEIGHT)).to eq(true)
       end
     end
   end
