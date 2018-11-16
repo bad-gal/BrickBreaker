d1be31dbfc18567d55acbe11b6dd6468af32256c
Andrien Ricketts <knowledgehut.developments@gmail.com>
2018-11-16 12:46:21 +0000

    Change the way the capsule class operates

--- lib/capsule.rb
+++ lib/capsule.rb
@@ -2,38 +2,32 @@ require 'byebug'
 class Capsule
   include Image
   attr_accessor :position, :velocity, :visible
-  attr_reader :width, :height, :image, :type
+  attr_reader :image, :type
 
   MOVE = 3
+  CAPSULE_WIDTH = 60
+  CAPSULE_HEIGHT = 16
+
   CAPSULES = %i[extra_life small_paddle large_paddle fast_ball slow_ball
                 score_250 score_100 score_500 multi wrap flip
                 laser gun bomb empty].freeze
 
-  def initialize(type:, position:)
+  def initialize(type:, x:, y:)
     @image = Image.create(file: acquire_filename(type))
     @type = type
-    @position = position
-    @width = 60
-    @height = 16
-    @velocity = [0, 0]
+    @position = { x: x, y: y }
+    @velocity = { x: 0, y: 0 }
     @visible = false
   end
 
-  def collides_with?(pos, width, height)
-    return if type == :empty
-
-    @position[0] >= pos[0] && @position[0] <= pos[0] + width &&
-      @position[1] >= pos[1] && @position[1] <= pos[1] + height
-  end
-
-  def fall
-    return if type == :empty
-
-    @position[1] += @velocity[1] if visible
-  end
-
   def draw
-    Image.draw(image: @image, position: @position, z: 0) if @visible
+    Image.draw(image: @image, position: @position) if @visible
+  end
+
+  def move
+    return if @type == :empty
+
+    @position[:y] += @velocity[:y] if @visible
   end
 
   def acquire_filename(type)
--- spec/capsule_spec.rb
+++ spec/capsule_spec.rb
@@ -2,7 +2,7 @@ require 'image'
 require 'capsule'
 
 RSpec.describe 'Capsule' do
-  let(:capsule) { Capsule.new(type: :score_250, position: [310, 210]) }
+  let(:capsule) { Capsule.new(type: :score_250, x: 310, y: 210) }
 
   describe '.initialize' do
     it 'is valid with the correct parameters' do
@@ -10,33 +10,15 @@ RSpec.describe 'Capsule' do
     end
   end
 
-  describe '.collides_with?' do
-    let(:paddle_asset) { { file: 'image.png', pixel_size: 90 } }
-    let(:paddle) { Paddle.new([300, 200], details: paddle_asset) }
-    context 'when a collision occurs' do
-      it 'returns true' do
-        expect(capsule.collides_with?(paddle.position, paddle.width, paddle.height)).to eq(true)
-      end
-    end
-    context 'when there is no collision' do
-      before do
-        paddle.position = [100, 100]
-      end
-      it 'returns fails' do
-        expect(capsule.collides_with?(paddle.position, paddle.width, paddle.height)).to eq(false )
-      end
-    end
-  end
-
-  describe '.fall' do
+  describe '.move' do
     context 'when capsule is set to fall' do
       before do
         capsule.visible = true
-        capsule.velocity[1] = 5
-        capsule.fall
+        capsule.velocity[:y] = 5
+        capsule.move
       end
       it 'changes the position of the capsule' do
-        expect(capsule.position).to eq([310, 215])
+        expect(capsule.position).to eq(x: 310, y: 215)
       end
     end
   end
